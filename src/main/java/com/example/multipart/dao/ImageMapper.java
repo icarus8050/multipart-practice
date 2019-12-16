a2d737097beabc0ed6f65a1085e2ca0e80b3c83f
icarus8050 <icarus8050@naver.com>
2019-12-16 22:50:08 +0900

    findAll interface

--- src/main/java/com/example/multipart/dao/ImageMapper.java
+++ src/main/java/com/example/multipart/dao/ImageMapper.java
@@ -3,7 +3,10 @@ package com.example.multipart.dao;
 import com.example.multipart.dto.ImageDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ImageMapper {
+    List<ImageDto> findAll();
     Long save(ImageDto imageDto);
 }
